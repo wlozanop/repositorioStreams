bf483525c4a1ce3d1380c69595d0ccd9273898c9
William Lozano <lozano.lpw@gmail.com>
2024-10-28 22:01:31 -0500

    Se actualizo la clase del Ejercicio5

--- src/main/java/usuario/Ejercicio5.java
+++ src/main/java/usuario/Ejercicio5.java
@@ -1,13 +1,22 @@
 package usuario;
 
-import parse.ParseUsuario;
+import leerArchivo.ExcellReader;
+import modelo.Usuario;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        final var mapUsuario = ParseUsuario.getUsuarioLista();
-        final var usuario1 = mapUsuario.get("USR-11");
-        final var usuario2 = mapUsuario.get("USR-19");
+        final var usuario = ExcellReader.getUsuariosList();
 
-        System.out.printf("Sumar edades: %d", usuario1.obtenerEdad() + usuario2.obtenerEdad());
+        final var map = usuario
+                .stream()
+                .collect(Collectors.toMap(Usuario::getId, Function.identity()));
+
+        final var usuario1 = map.get("USR-3");
+        final var usuario2 = map.get("USR-11");
+
+        System.out.printf("Suma de edades: %d", usuario1.getEdad() + usuario2.getEdad());
     }
 }
